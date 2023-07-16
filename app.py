1af9b1f4ab20cc5f7653cc37a795354c76889f08
karan3093 <119920105+karan3093@users.noreply.github.com>
2023-07-16 15:10:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,8 @@ async def hello():
     for url in urls:
         markup += f'<img src="{url}"></img><br><br>'
 
-
-    break
     return markup
 
 
 if __name__ == '__main__':
-    app.r
-    #app.run(debug=True)
-    
+    app.run(debug=True)
