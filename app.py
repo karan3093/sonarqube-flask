89f533763dca54012db1b38d4a6a420cd28cb88e
karan3093 <119920105+karan3093@users.noreply.github.com>
2023-07-18 15:16:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,23 @@ app = Flask(__name__)
 
 # function converted to coroutine
 async def get_xkcd_image(session):
-    """function converted to coroutine"""
+    """function converted to coroutine
+    :return: json format
+    :rtype: dict"""
     random = randint(0, 300)
     result = await session.get(f'http://xkcd.com/{random}/info.0.json')
     """dont wait for the response of API"""
     return result.json()['img']
 
 # function converted to coroutine
-async def get_multiple_images(number: int) -> list:
+async def get_multiple_images(number: int) -> List:
     """function converted to coroutine
     :param number: number
     :type number: int
     :return: list of data
-    :rtype: list"""
-    async with httpx.AsyncClient() as session: # async client used for async functions
+    :rtype: List"""
+    async with httpx.AsyncClient() as session: 
+        # async client used for async functions
         tasks = [get_xkcd_image(session) for _ in range(number)]
         result = await asyncio.gather(*tasks, return_exceptions=True)
         """gather used to collect all coroutines"""
@@ -30,8 +33,10 @@ async def get_multiple_images(number: int) -> list:
 
 
 @app.get('/comic')
-async def hello():
-    """function converted to coroutine"""
+async def hello() -> str:
+    """function converted to coroutine
+    :return: markup value
+    :rtype: str"""
     start = time.perf_counter()
     urls = await get_multiple_images(5)
     end = time.perf_counter()
