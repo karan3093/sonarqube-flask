3767983511e12579e1af911c6e0bf455242a477a
karan3093 <119920105+karan3093@users.noreply.github.com>
2023-07-19 14:35:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,55 @@
-"""This program is for achieveing some task"""
-import list
 import asyncio
 import time
 from random import randint
 import httpx
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# Mocked user data for authentication API
+users = {
+    "user1": {
+        "username": "user1",
+        "password": "password1"
+    },
+    "user2": {
+        "username": "user2",
+        "password": "password2"
+    }
+}
+
 # function converted to coroutine
-async def get_xkcd_image(session: str) -> any:
-    """function converted to coroutine
+async def get_xkcd_image(session: httpx.AsyncClient) -> str:
+    """Function converted to coroutine
     :param session: session to connect server
-    :type: str
-    :return: json format
-    :rtype: any"""
+    :type: httpx.AsyncClient
+    :return: URL of the XKCD image
+    :rtype: str"""
     random = randint(0, 300)
-    result = await session.get(f'http://xkcd.com/{random}/info.0.json')
-    """dont wait for the response of API"""
+    async with session:
+        result = await session.get(f'http://xkcd.com/{random}/info.0.json')
+    """Don't wait for the response of API"""
     return result.json()['img']
 
 # function converted to coroutine
-async def get_multiple_images(number: int) -> any:
-    """function converted to coroutine
+async def get_multiple_images(number: int) -> list:
+    """Function converted to coroutine
     :param number: number
     :type number: int
-    :return: list of data
-    :rtype: any"""
+    :return: List of image URLs
+    :rtype: list"""
     async with httpx.AsyncClient() as session:
-        # async client used for async functions
+        # Async client used for async functions
         tasks = [get_xkcd_image(session) for _ in range(number)]
         result = await asyncio.gather(*tasks, return_exceptions=True)
-        """gather used to collect all coroutines"""
+        """Gather used to collect all coroutines"""
     return result
 
 
 @app.get('/comic')
 async def hello() -> str:
-    """function converted to coroutine
-    :return: markup value
+    """Function converted to coroutine
+    :return: Markup value
     :rtype: str"""
     start = time.perf_counter()
     urls = await get_multiple_images(5)
@@ -47,6 +58,60 @@ async def hello() -> str:
     for url in urls:
         markup += f'<img src="{url}"></img><br><br>'
     return markup
+
+
+@app.post('/login')
+def login() -> str:
+    """API endpoint for user login
+    :return: JSON response with login status
+    :rtype: str"""
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username and password:
+        if username in users and users[username]['password'] == password:
+            return jsonify({'status': 'success', 'message': 'Login successful'})
+        else:
+            return jsonify({'status': 'error', 'message': 'Invalid username or password'})
+    else:
+        return jsonify({'status': 'error', 'message': 'Username and password are required'}), 400
+
+
+@app.get('/data')
+def get_data() -> str:
+    """API endpoint to retrieve data
+    :return: JSON response with data
+    :rtype: str"""
+    data = {
+        'key1': 'value1',
+        'key2': 'value2',
+        'key3': 'value3'
+    }
+    return jsonify(data)
+
+
+@app.post('/data')
+def create_data() -> str:
+    """API endpoint to create data
+    :return: JSON response with creation status
+    :rtype: str"""
+    # Process the request and create the data
+    # ...
+
+    return jsonify({'status': 'success', 'message': 'Data created'})
+
+
+@app.put('/data/<id>')
+def update_data(id: str) -> str:
+    """API endpoint to update data
+    :param id: ID of the data to update
+    :type id: str
+    :return: JSON response with update status
+    :rtype: str"""
+    # Process the request and update the data with the given ID
+    # ...
+
+    return jsonify({'status': 'success', 'message': f'Data with ID {id} updated'})
 
 
 if __name__ == '__main__':
