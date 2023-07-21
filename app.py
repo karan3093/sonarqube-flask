ccb3dea97e1abdbb518cbb20a986fec131bde0db
karan3093 <119920105+karan3093@users.noreply.github.com>
2023-07-21 11:06:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-"""This code shows some output"""
 import asyncio
 import time
 from random import randint
@@ -27,38 +26,38 @@ users = {
     }
 }
 
-async def get_xkcd_image(session: AsyncClient) -> any:
+async def get_xkcd_image(session: AsyncClient) -> str:
     """Get a random XKCD comic image URL asynchronously.
 
     :param session: httpx.AsyncClient session to connect to the server
     :type session: httpx.AsyncClient
     :return: URL of the XKCD image
-    :rtype: any
+    :rtype: str
     """
     random = randint(0, 300)
     async with session:
         result = await session.get(f'http://xkcd.com/{random}/info.0.json')
     return result.json()['img']
 
-async def get_multiple_images(number: int) -> any:
+async def get_multiple_images(number: int) -> list:
     """Get multiple XKCD comic image URLs asynchronously.
 
     :param number: Number of comic images to fetch
     :type number: int
     :return: List of image URLs
-    :rtype: any
+    :rtype: list
     """
     async with AsyncClient() as session:
         tasks = [get_xkcd_image(session) for _ in range(number)]
         result = await asyncio.gather(*tasks, return_exceptions=True)
     return result
 
-@app.get('/comic')
-async def get_comic_images() -> any:
+@app.route('/comic')
+async def get_comic_images() -> str:
     """Get multiple XKCD comic images and display them.
 
     :return: Markup containing comic images and time taken
-    :rtype: any
+    :rtype: str
     """
     start = time.perf_counter()
     urls = await get_multiple_images(5)
@@ -68,7 +67,7 @@ async def get_comic_images() -> any:
         markup += f'<img src="{url}"></img><br><br>'
     return markup
 
-@app.post('/login')
+@app.route('/login', methods=['POST'])
 def login() -> any:
     """API endpoint for user login.
 
@@ -86,12 +85,12 @@ def login() -> any:
     else:
         return jsonify({'status': 'error', 'message': 'Username and password are required'}), 400
 
-@app.get('/data')
-def get_data() -> any:
+@app.route('/data', methods=['GET'])
+def get_data() -> str:
     """API endpoint to retrieve data.
 
     :return: JSON response with data
-    :rtype: any
+    :rtype: str
     """
     data = {
         'key1': 'value1',
@@ -100,25 +99,25 @@ def get_data() -> any:
     }
     return jsonify(data)
 
-@app.post('/data')
-def create_data() -> any:
+@app.route('/data', methods=['POST'])
+def create_data() -> str:
     """API endpoint to create data.
 
     :return: JSON response with creation status
-    :rtype: any
+    :rtype: str
     """
     # Process the request and create the data
     # ...
     return jsonify({'status': 'success', 'message': 'Data created'}), 201
 
-@app.put('/data/<id>')
-def update_data(id: str) -> any:
+@app.route('/data/<id>', methods=['PUT'])
+def update_data(id: str) -> str:
     """API endpoint to update data.
 
     :param id: ID of the data to update
     :type id: str
     :return: JSON response with update status
-    :rtype: any
+    :rtype: str
     """
     # Process the request and update the data with the given ID
     # ...
@@ -128,7 +127,7 @@ def update_data(id: str) -> any:
 def search() -> any:
     """API endpoint to search for user details.
 
-    :return: JSON response with update status
+    :return: JSON response or rendered HTML template with user details
     :rtype: any
     """
     if request.method == "POST":
@@ -137,85 +136,55 @@ def search() -> any:
         cursor.execute('SELECT Name, Email, Experience, Skill_Set FROM details WHERE Name = %s', [group])
         data = cursor.fetchone()
         cursor.close()
+
         if data is None:
-            return None
-        return True
-    return True
+            return "NO DETAILS FOUND", 404
+
+        dic = {
+            'Name': data[0],
+            'Email': data[1],
+            'Experience': data[2],
+            'Skills': data[3]
+        }
+        return render_template('search.html', dic=dic)
 
 class AppTestCase(unittest.TestCase):
-    """Method for APP Test Case"""
 
-    def setUp(self) -> any:
-        """Method to setup
-
-        :return: JSON response or rendered HTML template with user details
-        :rtype: any
-        """
+    def setUp(self):
         self.app = app.test_client()
         # Add any setup actions here, such as creating a test database or test data.
-        return True
 
-    def tearDown(self) -> any:
-        """Method to teardown
-
-        :return: JSON response or rendered HTML template with user details
-        :rtype: any
-        """
+    def tearDown(self):
         # Add any cleanup actions here, such as closing the test database connection.
-        return True
+        pass
 
-    def test_comic_route(self) -> any:
-        """Method to test comic route
-
-        :return: JSON response or rendered HTML template with user details
-        :rtype: any
-        """
+    def test_comic_route(self):
+        """Test the /comic route to get multiple XKCD comic images."""
         response = self.app.get('/comic')
         self.assertEqual(response.status_code, 200)
         # Add more specific assertions for the content of the response.
-        return response
 
-    def test_login_route(self) -> any:
-        """Test the login route for user login.
-
-        :return: JSON response or rendered HTML template with user details
-        :rtype: any
-        """
-        data={'username': 'user1', 'password': 'password1'}
-        login_page = '/login'
-        response = self.app.post(login_page, data)
+    def test_login_route(self):
+        """Test the /login route for user login."""
+        response = self.app.post('/login', data={'username': 'user1', 'password': 'password1'})
         self.assertEqual(response.status_code, 200)
         # Add more specific assertions for the JSON response.
 
-        response = self.app.post(login_page, data={'username': 'user1', 'password': 'wrong_password'})
+        response = self.app.post('/login', data={'username': 'user1', 'password': 'wrong_password'})
         self.assertEqual(response.status_code, 401)
         # Test for invalid password
 
-        response = self.app.post(login_page, data={'username': 'non_existent_user', 'password': 'password'})
+        response = self.app.post('/login', data={'username': 'non_existent_user', 'password': 'password'})
         self.assertEqual(response.status_code, 401)
         # Test for non-existent user
 
-    def test_get_xkcd_image(self) -> any:
-        """Test the get_xkcd_image function to retrieve a random XKCD comic image URL.
-
-        :return: JSON response or rendered HTML template with user details
-        :rtype: any
-        """
-        random_number = 42
-        # Replace with any desired random number
-        _ = f'http://xkcd.com/{random_number}/info.0.json'
+    def test_get_xkcd_image(self):
+        """Test the get_xkcd_image function to retrieve a random XKCD comic image URL."""
+        random_number = 42  # Replace with any desired random number
+        expected_url = f'http://xkcd.com/{random_number}/info.0.json'
         json_response = {'img': 'https://example.com/comic.png'}
 
-        async def mock_get(*args: any, **kwargs: any) -> any:
-            """Test the get_xkcd_image function to retrieve a random XKCD comic image URL.
-
-            :param *args: ID of the data to update
-            :type *args: any
-            :param **kwargs: ID of the data to update
-            :type **kwargs: any
-            :return: JSON response with update status
-            :rtype: any
-            """
+        async def mock_get(*args, **kwargs):
             response = mock.Mock()
             response.json.return_value = json_response
             return response
@@ -224,12 +193,8 @@ class AppTestCase(unittest.TestCase):
             result = asyncio.run(get_multiple_images(1))
             self.assertEqual(result[0], json_response['img'])
 
-    def test_search_route(self) -> any:
-        """Test the /search route to search for user details.
-
-        :return: JSON response or rendered HTML template with user details
-        :rtype: any
-        """
+    def test_search_route(self):
+        """Test the /search route to search for user details."""
         with app.test_client() as client:
             response = client.post('/search', data={'search': 'John Doe'})
             self.assertEqual(response.status_code, 404)
@@ -237,7 +202,6 @@ class AppTestCase(unittest.TestCase):
             # Test for a user that does not exist in the database.
 
             # Add more test cases for the 'search' route as needed.
-            return response
 
 if __name__ == '__main__':
     unittest.main()
